f7b618512a6313f2a292e4e00018148fdc7b85f3
sjextlrodriguez <sjextlrodriguez@micoope.coosajo.com>
2021-10-11 16:13:43 -0600

    Commit comunicacion VUE

---
+++ src/components/Datos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-body">
+                <h4>{{ DatosForm.Nombre }}</h4>
+                <button class="btn btn-warning" @click="Editar">Editar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Datos',
+    props : {
+        DatosForm : Object
+    },
+    methods : {
+        Editar(){
+            this.$emit('Edicion', false);
+        }
+    }
+}
+</script>
